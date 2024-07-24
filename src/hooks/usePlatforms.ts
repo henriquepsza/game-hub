fc9473ecfb7ef340a726db6199283a02f8622e2d
Henrique Pereira <henriquepsz@protonmail.com>
2024-07-24 18:04:46 -0300

    Refactoring: fetch games with react-query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,5 @@
-import { FetchResponse } from "./useData.ts";
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client.ts";
+import apiClient, { FetchResponse } from "../services/api-client.ts";
 import platforms from "../data/platforms.ts";
 
 interface Platform {
